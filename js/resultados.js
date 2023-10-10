98921c01341779989cf089c6624cbcf55eb9d096
AechBr <121638449+AechBr@users.noreply.github.com>
2023-10-09 21:44:08 -0300

    Add files via upload

---
+++ js/resultados.js
@@ -0,0 +1,12 @@
+// Comando 1
+var data = {
+    livros: ['Orange is The New Black - Piper Kerman', 'A Origem das Espécies - Charles Darwin'],
+    MetaGerencial: 'Livros 2'
+  };
+  // Comando 2
+  new Vue({
+    el: '#resultados',
+    data: data
+  });
+  
+  data.MetaGerencial="R$ 908.990"
